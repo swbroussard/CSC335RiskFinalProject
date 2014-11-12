81483882394b841846eee994a2379446dfb122c6
Steven Wilson Broussard <stevenwbroussard@corgi.cs.arizona.edu>
2014-11-11 17:21:24 -0700

    added a file to test the simple AI player and simple AI program (I did not saw there was another simple AI java file already created and I deleted it SimpleAIPlayer.java, sorry), and added a new folder called test. -steven

---
+++ src/model/SimpleAI.java
@@ -0,0 +1,20 @@
+package model;
+
+public class SimpleAI extends Player{
+
+	@Override
+	public void playTurn() {
+		
+		//makes decision according to a random number generator
+		//for test only, the decision will be static
+		
+	}
+
+	@Override
+	public void chooseTerritory() {
+		
+		//makes decision according to a random number generator
+		//for test only, the decision will be static
+		
+	}
+}
---
+++ src/test/testPlayerSimpleAI.java
@@ -0,0 +1,13 @@
+package testModelPrograms;
+
+import org.junit.Test;
+import model.SimpleAI;
+
+public class testPlayerSimpleAI {
+
+	@Test
+	public void testTheAIExist(){
+		SimpleAI sim = new SimpleAI();
+	}
+
+}
