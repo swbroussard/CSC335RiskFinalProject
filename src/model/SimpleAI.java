45d1d4ca21a915645a42fac3533e89390e4ec523
Steven Wilson Broussard <stevenwbroussard@afghan.cs.arizona.edu>
2014-11-12 16:25:43 -0700

    worked on the SimpleAI.java and testPlayerSimpleAI.java. -steven

--- src/model/SimpleAI.java
+++ src/model/SimpleAI.java
@@ -1,9 +1,23 @@
 package model;
 
+import java.util.Random;
+
 public class SimpleAI extends Player{
+	
+	//instance variables
+	Random randomGen;
+	
+	//constructor
+	public SimpleAI(){
+		randomGen = new Random(1);  //static state from the random generator
+		//randomGen = new Random();  //random generator
+	}
 
 	@Override
 	public void playTurn() {
+		
+		//-steven, makes an random decision to attack or not
+		//also to reinforce troops or not
 		
 		//makes decision according to a random number generator
 		//for test only, the decision will be static
@@ -12,6 +26,8 @@ public class SimpleAI extends Player{
 
 	@Override
 	public void chooseTerritory() {
+		
+		//-steven, makes random decision to choose which territory
 		
 		//makes decision according to a random number generator
 		//for test only, the decision will be static
--- src/test/testPlayerSimpleAI.java
+++ src/test/testPlayerSimpleAI.java
@@ -2,12 +2,26 @@ package test;
 
 import org.junit.Test;
 import model.SimpleAI;
+import model.Player;
 
 public class testPlayerSimpleAI {
 
 	@Test
 	public void testTheAIExist(){
-		SimpleAI sim = new SimpleAI();
+		Player player1 = new SimpleAI();
+	}
+	
+	@Test
+	public void testPlayTurnMethod(){
+		Player player1 = new SimpleAI();
+		//test if it will attack or not using the static state random generator
+		//test if it will reinforce or not using the static state random generator
+	}
+	
+	@Test
+	public void testChooseTerritoryMethod(){
+		Player player1 = new SimpleAI();
+		//test if it chooses the correct territory using the static state random generator
 	}
 
 }
