ea6e1b894462c93a0f74d0107148e3de1399aca8
Rebecca Simon <Becca@Rebeccas-MacBook-Pro.local>
2014-11-16 00:43:53 -0700

    My GUI updates for tonight

--- src/controller/RiskGUI.java
+++ src/controller/RiskGUI.java
@@ -1,34 +1,77 @@
 package controller;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class RiskGUI extends JFrame{
 	private RiskController controller;
 	private JFrame frame;
+	private JPanel buttonPanel, mapPanel, labelPanel;
+	
 	
 	public RiskGUI() {
-		controller = new RiskController();
+		//controller = new RiskController();
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.setSize(800, 800);
+		frame.setSize(1100, 800);
 		frame.setTitle("RISK");
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(new BorderLayout());
+		JPanel basePanel = new JPanel();
+		basePanel.setLayout(new BorderLayout());
 		
-		JPanel panel2 = new JPanel();
-		panel2.setLayout(new BorderLayout());
+		JPanel mapAndLabelPanel = new JPanel();
+		mapAndLabelPanel.setLayout(new BorderLayout());
 		
-		JPanel panel3 = new JPanel();
-		panel3.setLayout(new BorderLayout());
+		setUpMapPanel();
+		mapAndLabelPanel.add(mapPanel, BorderLayout.CENTER);
 		
-		panel2.add(panel3, BorderLayout.CENTER);
-		panel.add(panel2, BorderLayout.CENTER);
+		setUpButtonSidePanel();
+		basePanel.add(buttonPanel, BorderLayout.EAST);
 		
+		setUpLabelPanel();
+		mapAndLabelPanel.add(labelPanel, BorderLayout.SOUTH);
+		
+		basePanel.add(mapAndLabelPanel, BorderLayout.CENTER);
+		
+		frame.add(basePanel);
 		frame.setVisible(true);
+	}
+	
+	public void setUpButtonSidePanel() {
+		buttonPanel = new JPanel();
+		buttonPanel.setPreferredSize(new Dimension(200, 800));
+		buttonPanel.setBackground(Color.GREEN);
+		buttonPanel.setVisible(true);
+	}
+	
+	public void setUpMapPanel() {
+		mapPanel = new JPanel();
+		mapPanel.setPreferredSize(new Dimension(300, 700));
+		mapPanel.setBackground(Color.RED);
+		mapPanel.setVisible(true);
+	}
+	
+	public void setUpLabelPanel() {
+		labelPanel = new JPanel();
+		labelPanel.setPreferredSize(new Dimension(900, 50));
+		//labelPanel.setBackground(Color.BLUE);
+		labelPanel.setVisible(true);
+		
+		JLabel label = new JLabel();
+		label.setSize(900, 50);
+		label.setFont(new Font("Arial", Font.BOLD, 16));
+		label.setHorizontalTextPosition(SwingConstants.CENTER);
+		label.setForeground(Color.BLACK);
+		label.setText("You have started a new game!");
+		
+		labelPanel.add(label);
 	}
 	
 	public static void main(String[] args) {
