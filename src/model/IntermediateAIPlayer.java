e50ae6cbed22c034c2477f22798fdf1e0e4bcf0e
jjalnos <jeremyjalnos@JeremykJalnos.local>
2014-11-20 14:13:01 -0700

    modified intermediate AI

--- src/model/IntermediateAIPlayer.java
+++ src/model/IntermediateAIPlayer.java
@@ -27,26 +27,34 @@ public class IntermediateAIPlayer extends Player{
 
 	
 	
-	
-	
 	/**
 	 * TODO: Fix all Jeremy's nonSense below 
 	 */
 	
 	int numTroopsTake = 0;
+	int numTroopsTake2 = 0;
 	
 	public void reinforce(){
 	Territory high = null; 
+	Territory high2 = null;
 	Territory low = null;
+	Territory low2 = null;
 	int highTroop = 0;
+	int highTroop2 =0;
 	int highAdjacent = 0;
+	int highAdjacent2 = 0;
 	int lowTroop = 0;
+	int lowTroop2 =0;
 	
 	for(int i =0; i <= getTerritories().size(); i++){
 		for(int j =0; j <= getTerritories().get(i).getAdjacent().size(); j++){// check all territories look through all armies find lowest with competitors or ones with no competitors and give to territories more in need
 			if(getTerritories().get(i).getAdjacent().get(j).getCurrentOwner() != this){
 			if(getTerritories().get(i).getAdjacent().get(j).getNumArmies() >= getTerritories().get(i).getNumArmies()){
 				low = getTerritories().get(i);
+			}
+			if(getTerritories().get(i).getAdjacent().get(j).getNumArmies() >= getTerritories().get(i).getNumArmies() && getTerritories().get(i).getNumArmies() >= low.getNumArmies()){
+				low2 = getTerritories().get(i);
+				lowTroop2 = getTerritories().get(i).getNumArmies();
 			}
 			else{
 				highAdjacent = getTerritories().get(i).getAdjacent().get(j).getNumArmies();
