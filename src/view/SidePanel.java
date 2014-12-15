6c7adfda4ae39d3220f551b11bf2e230c4d9fe69
Rebecca Simon <Becca@Rebeccas-MacBook-Pro.local>
2014-12-14 20:08:51 -0700

    trying to fix Jeremy's computer

--- src/view/SidePanel.java
+++ src/view/SidePanel.java
@@ -26,7 +26,7 @@ import model.Player;
  */
 @SuppressWarnings("serial")
 public class SidePanel extends JPanel{
-	private JButton doneAttacking, doneFortifying, exit;
+	private JButton doneAttacking, exit;
 	private RiskGUI gui;
 	private BufferedImage star;
 	
@@ -49,18 +49,11 @@ public class SidePanel extends JPanel{
 		this.setVisible(true);
 		
 		doneAttacking = new JButton("Done Attacking");
-		doneFortifying = new JButton("Done Fortifying");
 		exit = new JButton("Exit Game");
 		
 		doneAttacking.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				doneAttacking();
-			}
-		});
-		
-		doneFortifying.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				doneFortifying();
 			}
 		});
 		
@@ -72,7 +65,6 @@ public class SidePanel extends JPanel{
 		
 		
 		this.add(doneAttacking);
-		this.add(doneFortifying);
 		this.add(exit);
 		repaint();
 	}
@@ -112,26 +104,32 @@ public class SidePanel extends JPanel{
 			g2.drawString(p.getName(), 41, yPos);
 			yPos += 20;
 		}
-		
+		yPos = 156;
 		
 		switch(gui.getPlayers().indexOf(gui.getCurrentPlayer())) {
 			case 0: // player 1
-				g2.drawImage(star, 25, 156, 15, 15, null); 
+				g2.drawImage(star, 25, yPos, 15, 15, null); 
+				yPos += 20;
 				break;
 			case 1: // player 2
-				g2.drawImage(star, 25, 176, 15, 15, null); 
+				g2.drawImage(star, 25, yPos, 15, 15, null); 
+				yPos += 20;
 				break;
 			case 2: // player 3
-				g2.drawImage(star, 25, 196, 15, 15, null); 
+				g2.drawImage(star, 25, yPos, 15, 15, null); 
+				yPos += 20;
 				break;
 			case 3: // player 4
-				g2.drawImage(star, 25, 216, 15, 15, null); 
+				g2.drawImage(star, 25, yPos, 15, 15, null); 
+				yPos += 20;
 				break;
 			case 4: // player 5
-				g2.drawImage(star, 25, 236, 15, 15, null); 
+				g2.drawImage(star, 25, yPos, 15, 15, null); 
+				yPos += 20;
 				break;
 			case 5: // player 6
-				g2.drawImage(star, 25, 256, 15, 15, null); 
+				g2.drawImage(star, 25, yPos, 15, 15, null); 
+				yPos += 20;
 				break;
 		}
 		g2.setColor(Color.BLACK);
@@ -142,5 +140,8 @@ public class SidePanel extends JPanel{
 		g2.drawString("Africa - 3", 40, 380);
 		g2.drawString("Asia - 7", 40, 400);
 		g2.drawString("Australia - 2", 40, 420);
+		
+		g2.drawString("If you do not want to reinforce at the end of your turn, "
+				+ "select a to and from territory, then enter 0 for the number of armies", 10, 440);
 	}
 }
